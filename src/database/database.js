769655f88738cfa3394c5254f03e09873ca51332
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-02-04 15:46:06 -0500

    conect to postgres

--- src/database/database.js
+++ src/database/database.js
@@ -2,13 +2,4 @@ import Sequelize from "sequelize";
 import { config } from "dotenv";
 config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  process.env.PORT,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+export const sequelize = new Sequelize(process.env.DB_URL); // Example for postgres
